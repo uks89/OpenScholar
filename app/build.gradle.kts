b242e55ed5d93d329c908b57c6e72d709f7ba7e1
Uttam <uttamkumarsarker.sust.phy@gmail.com>
2026-06-06 09:40:17 +0600

    Fix Hilt KSP artifact: use hilt-compiler instead of hilt-android-compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
 
     // Hilt DI
     implementation("com.google.dagger:hilt-android:2.48.1")
-    ksp("com.google.dagger:hilt-android-compiler:2.48.1")
+    ksp("com.google.dagger:hilt-compiler:2.48.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     // Room Database
